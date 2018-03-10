ac52c08a9a3a75a12bc7f398773f026a70126d4d
maxravel <maxmaterniak@gmail.com>
2018-03-10 20:40:56 +0100

    pure js upradges

--- js.js
+++ js.js
@@ -1,21 +1,46 @@
-/*function shadow(){
-    //document.getElementById("test1").style.color="black";
-    document.getElementById("test1").className = "shadow";
-    //document.getElementById('test1').classList.add('shadow');
-};
-*/
-
 $(document).ready(function(){
-    $("#shd").click(function(){
-        $("#test1").toggleClass("shadow");
-        $("#shd").toggleClass("btnc");
+
+    // $("#shd").click(function(){
+    //     $("#test1").toggleClass("shadow");
+    //     $("#shd").toggleClass("btnc");
+    // });
+
+
+    // PURE JS CODE
+
+    const shd = document.querySelector('#shd');
+    const test1 = document.querySelector('#test1');
+
+    // VERSION 1 - LONG ONE
+    // shd.addEventListener('click', function(){
+    //     if(shd.classList.contains('btnc')){
+    //        shd.classList.remove('btnc');
+    //        test1.classList.remove('shadow');
+    //     }
+    //     else{
+    //        shd.classList.add('btnc');
+    //        test1.classList.add('shadow');
+    //     }
+    // });
+
+    // VERSION 2 - SHORT ONE
+    shd.addEventListener('click', function(){
+        
+           shd.classList.toggle('btnc');
+           test1.classList.toggle('shadow');
+     
     });
+    
+
     $("#tran").click(function(){
         $("#test1").toggleClass("transit");
         $("#tran").toggleClass("btnt");
     });
+
     $("#grad").click(function(){
         $(".mainSection").toggleClass("mainSectionG");
         $("#grad").toggleClass("btnc");
     });
+
 });
+
