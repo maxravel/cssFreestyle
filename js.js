48a4345cd0af87d1034a684ee50e6c2ab51b1818
maxravel <maxmaterniak@gmail.com>
2018-07-04 22:14:24 +0200

    infinite color changer, works by js

--- js.js
+++ js.js
@@ -45,11 +45,19 @@ $(document).ready(function(){
             testT.classList.remove('test1l');
             testT.classList.add('test1r');
             transformB.classList.remove('btnt');
+
+            setTimeout(function(){
+                testT.classList.remove('test1r');
+            },3000)
         }
         else{
+            testT.classList.remove('test1r');
             testT.classList.add('test1l');
             transformB.classList.add('btnt');
-            testT.classList.remove('test1r');
+            
+            setTimeout(function(){
+                testT.classList.remove('test1l');
+            },3000)
         }
 
     });
@@ -75,21 +83,42 @@ $(document).ready(function(){
     //after clicking infinite and transform, when removing infinite testText is going up and down
     const infiniteB = document.querySelector("#infiniteButton");
 
+    var changer;
     infiniteB.addEventListener("click", function(){
 
-        if(infiniteB.classList.contains('btnc')){
-            testT.classList.remove('test1infinite');
-            infiniteB.classList.remove('btnc');
-        }
-        else{
-            testT.classList.add('test1infinite');
-            infiniteB.classList.add('btnc');
-        }
+        // XXXXXXXXXXXXXXXXXXXXX CSS version don't working well, after first using infinite
+        // if(infiniteB.classList.contains('btnc')){
+        //     testT.classList.remove('test1infinite');
+        //     infiniteB.classList.remove('btnc');
+        // }
+        // else{
+        //     testT.classList.add('test1infinite');
+        //     infiniteB.classList.add('btnc');
+        // }
 
         // infiniteB.classList.toggle('btnc');
         // testT.classList.toggle('test1infinite');
+        
+        if(infiniteB.classList.contains('btnc')){
+            clearInterval(changer);
+            infiniteB.classList.remove('btnc');
+            testT.style.color = "#530853";
+        }
+        else{
+            changer = setInterval(colorChange, 1000);
+            infiniteB.classList.add('btnc');
+            colors = ["black", "green", "blue", "pink"];
+        }
 
     })
-
+    let colors;
+    let i = 0;
+    function colorChange(){
+        if(i===colors.length){
+            i=0;
+        }
+        testT.style.color = colors[i];
+        i++;
+    }
 });
 
